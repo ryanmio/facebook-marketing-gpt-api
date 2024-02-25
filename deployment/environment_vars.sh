da1f91f2beea177188b3a19e8e43532a2251e330
ryanmio <ryan@mioduski.us>
2024-02-24 19:29:36 -0500

    Update environment_vars.sh

--- deployment/environment_vars.sh
+++ deployment/environment_vars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 export FACEBOOK_API_ACCESS_TOKEN='Your_Facebook_API_Access_Token'
+gcloud functions deploy YOUR_FUNCTION_NAME --runtime python39 --trigger-http --set-env-vars FACEBOOK_API_ACCESS_TOKEN=Your_New_Token
